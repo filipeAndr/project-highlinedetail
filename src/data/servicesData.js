dd7c84991efb3f683da635b11e557b9d38c07b8a
filipeAndr <filipe_andrade@ufms.br>
2026-04-11 01:56:10 -0400

    Atualização imagens dos cards de serviço

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -6,7 +6,7 @@ export const services = [
         title: "Pacote Bronze",
         subtitle: "Limpeza Rápida & Eficiente",
         price: { hatch: "320,00", sedan: "360,00", suv: "480,00", camionete: "600,00", luxo: "760,00" },
-        image: "./src/assets/img/fig4.png", // Troque pela foto do Fast
+        image: "./src/assets/img/pacote-bronze.jpeg", // Troque pela foto do Fast
         description: [
             "Limpeza Simples",
             "Aromatizante",
@@ -19,7 +19,7 @@ export const services = [
         title: "Pacote Prata",
         subtitle: "O campeão de vendas",
         price: { hatch: "350,00", sedan: "405,00", suv: "545,00", camionete: "680,00", luxo: "880,00" },
-        image: "./src/assets/img/fig2.png", // Troque pela foto do produto azul (Vonixx)
+        image: "./src/assets/img/pacote-prata.jpeg", // Troque pela foto do produto azul (Vonixx)
         description: [
             "Limpeza Simples",
             "Aromatizante",
@@ -33,7 +33,7 @@ export const services = [
         title: "Pacote Ouro",
         subtitle: "Correção e Brilho",
         price: { hatch: "850,00", sedan: "1110,00", suv: "1370,00", camionete: "1670,00", luxo: "2380,00" },
-        image: "./src/assets/img/fig1.png", // Troque pela foto da politriz
+        image: "./src/assets/img/pacote-ouro.jpeg", // Troque pela foto da politriz
         description: [
             "Limpeza Detalhada",
             "Aromatizante",
