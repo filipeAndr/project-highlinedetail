e57e9e193466d4a8329f17e9f8c2842e99854086
filipeAndr <filipe_andrade@ufms.br>
2026-04-11 00:27:42 -0400

    correção - bug de imagem parceiros

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -11,7 +11,7 @@ class AppBrands extends HTMLElement {
             { name: "Cadillac", src: "./src/assets/parceiros/cadillac.png" },
             { name: "Carpro", src: "./src/assets/parceiros/carPro.png" },
             { name: "EasyTech", src: "./src/assets/parceiros/easytech.png" },
-            { name: "KCX", src: "./src/assets/parceiros/kcx.png" },
+            { name: "KCX", src: "./src/assets/parceiros/kcx.jpeg" },
             { name: "NobreCar", src: "./src/assets/parceiros/nobrecar.png" },
             { name: "Soft99", src: "./src/assets/parceiros/soft99.png" },
             { name: "Sonax", src: "./src/assets/parceiros/sonax.png" },
@@ -22,7 +22,7 @@ class AppBrands extends HTMLElement {
 
         const brandsHTML = brands.map(brand => `
             <div class="group bg-[#121212] border border-[#2B2B2B] hover:border-[#D90429] aspect-square flex items-center justify-center p-6 transition-all duration-500 hover:-translate-y-2 hover:shadow-[0_10px_30px_rgba(217,4,41,0.15)] cursor-pointer">
-                <img src="${brand.src}" alt="${brand.name}" class="max-h-32 md:max-h-32 w-auto object-contain  group-hover:opacity-100 group-hover:grayscale-0 group-hover:brightness-100 transition-all duration-500 transform group-hover:scale-110">
+                <img src="${brand.src}" alt="${brand.name}" class="max-h-32 md:max-h-32 w-auto object-contain brightness-150 drop-shadow-[0_10px_25px_rgba(217,4,41,0.8)] group-hover:opacity-100 group-hover:grayscale-0 group-hover:brightness-100 transition-all duration-500 transform group-hover:scale-110">
             </div>
         `).join('');
 
